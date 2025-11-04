b94c7196aa72ba58473cb3178a28ecb430e8c780
Fikri Ahmad <rukimevick@gmail.com>
2025-11-04 15:04:57 +0700

    Add PlayerData class for player state management
    Introduces a serializable PlayerData class to store player-related information such as name, stats, experience, and save time. This will facilitate saving and loading player progress in the game.

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+using System;
+
+[Serializable]
+public class PlayerData
+{
+    public string playerName;
+    public PlayerScript playerScript;
+    public int Health;
+    public int Heart;
+    public int Energy;
+    public int AtkPower;
+    public int DefPower;
+    public int ExpGain;
+    public int Coin;
+    public int Level;
+    public int Progress;
+    public string saveTime;
+}
