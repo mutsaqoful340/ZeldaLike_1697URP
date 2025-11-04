b31a434f3a69a470c306eed243cd220c16fa5ff9
Fikri Ahmad <rukimevick@gmail.com>
2025-11-04 15:03:15 +0700

    Add MainMenuSelector script for main menu UI
    Introduces MainMenuSelector.cs to handle main menu interactions, including play, load, and confirmation panels. This script manages UI panels and scene transitions in the Unity project.

---
+++ Assets/Scripts/MainMenuSelector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSelector : MonoBehaviour
+{
+    public GameObject panelSaves;
+    public GameObject panelConfirm;
+
+    public TextMeshProUGUI[] slotTexts = new TextMeshProUGUI[3];
+    private int selectedSlot = -1;
+
+    private void Start()
+    {
+        panelSaves.SetActive(false);
+        panelConfirm.SetActive(false);
+    }
+
+    public void OnClick_Play(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void OnClick_Load()
+    {
+        panelSaves.SetActive(true);
+    }
+
+    public void OnClick_ConfirmYES()
+    {
+        panelConfirm.SetActive(false);
+    }
+
+    public void OnClick_ConfirmNO()
+    {
+        panelConfirm.SetActive(false);
+    }
+}
